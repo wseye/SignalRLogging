1b3898b41ff3bcffd0ed52294dcff04ec34101cb
Wouter Seye <wouter.seye@gmail.com>
2013-03-28 08:30:51 +0100

    copied project to repo

---
+++ signalRLogging2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="signalRLogging2.MvcApplication" Language="C#" %>
---
+++ signalRLogging2/LogHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace signalRLogging2
+{
+    public class LogHub : Hub
+    {
+        public void Begin()
+        {
+            Task.Factory.StartNew(() =>
+                                      {
+                                          for (int i = 0; i < 10; i++)
+                                          {
+                                              Send(string.Format("message {0}", i));
+                                          }
+                                      });
+        }
+
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+    }
+}
